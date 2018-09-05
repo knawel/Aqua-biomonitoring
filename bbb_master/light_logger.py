59325d50f5b15eb4a59f1321be4846b73ef9cc8e
Dima <ds@beaglebone.localdomain>
2018-09-06 02:21:45 +0300

    Light logger: version 0.01

--- bbb_master/light_logger.py
+++ bbb_master/light_logger.py
@@ -1,14 +1,24 @@
-import Adafruit_BBIO.GPIO as GPIO
+#ver. 0.01
+#import Adafruit_BBIO.GPIO as GPIO
+import Adafruit_BBIO.ADC as ADC
 import time
-i = 30
-P = "P8_10"
-print GPIO
+import datetime
+import sys
 
-GPIO.setup(P, GPIO.OUT)
+#setup and constants
+ADC.setup()
+P_data = "P9_40"
+#f_log = "../../data_logs/light.log"
+f_log = "light.log"
+i = 500
+#main
+
 
 for j in range(i):
-	GPIO.output(P, GPIO.HIGH)
-	time.sleep(0.4)
-	GPIO.output(P, GPIO.LOW)
-	time.sleep(0.4)
-GPIO.cleanup()
+
+    with open(f_log, 'a') as iFile:
+        iFile.write(str(datetime.datetime.now())+\
+ "\t" + str(ADC.read(P_data))+"\n")
+    time.sleep(1200)
+    sys.stderr.write(str(j)+ "\n")
+
