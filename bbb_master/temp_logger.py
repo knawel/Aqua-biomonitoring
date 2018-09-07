4ee7bf54a4d7c00d184c98af45792d515f71c228
dima <knawell@gmail.com>
2018-09-07 12:57:13 +0300

    Add to temp logger mean temperature instead of one measurement

--- bbb_master/temp_logger.py
+++ bbb_master/temp_logger.py
@@ -1,4 +1,4 @@
-#ver. 0.01
+#ver. 0.02
 import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.ADC as ADC
 import time
@@ -11,6 +11,7 @@ P_temp = "P9_37"
 #f_log = "../../data_logs/light.log"
 f_log = "temp.log"
 i = 500
+n = 3 # number of measurements
 #main
 
 
@@ -18,7 +19,12 @@ for j in range(i):
 
     with open(f_log, 'a') as iFile:
         time_ = str(datetime.datetime.now())
-        temp_ = str(ADC.read(P_temp) * 0.18 * 1000)
+
+        for i in range(n):
+            t += ADC.read(P_temp) * 0.18 * 1000
+
+
+        temp_ = str(t/n)
         iFile.write(time_ + "\t" + temp_ +"\n")
     time.sleep(12)
     sys.stderr.write(str(j)+ "\n")
