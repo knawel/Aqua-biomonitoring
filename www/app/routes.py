3953c0616ff90be25b4eaacf1650681e36eddcc1
dima <knawell@gmail.com>
2018-09-18 00:04:40 +0300

    minor editions in  the flask

--- www/app/routes.py
+++ www/app/routes.py
@@ -1,19 +1,43 @@
 from app import app
 from flask import render_template
-from flask import Flask, render_template
-import matplotlib.pyplot as plt
-import io
-import base64
+import mpld3
+import numpy as np
+
+import matplotlib as plt
+
+import random
 
 
-def build_graph(x_coordinates, y_coordinates):
-    img = io.BytesIO()
-    plt.plot(x_coordinates, y_coordinates)
-    plt.savefig(img, format='png')
-    img.seek(0)
-    graph_url = base64.b64encode(img.getvalue()).decode()
-    plt.close()
-    return 'data:image/png;base64,{}'.format(graph_url)
+x = range(100)
+y = [a * 2 + random.randint(-20, 20) for a in x]
+
+def draw_fig(fig_type):
+    """Returns html equivalent of matplotlib figure
+    Parameters
+    ----------
+    fig_type: string, type of figure
+            one of following:
+                    * line
+                    * bar
+    Returns
+    --------
+    d3 representation of figure
+    """
+
+    fig, ax = plt.subplots()
+    if fig_type == "line":
+        ax.plot(x, y)
+    elif fig_type == "bar":
+        ax.bar(x, y)
+    elif fig_type == "scatter":
+        ax.scatter(x, y)
+    elif fig_type == "hist":
+        ax.hist(y, 10, normed=1)
+    elif fig_type == "area":
+        ax.plot(x, y)
+        ax.fill_between(x, 0, y, alpha=0.2)
+
+    return mpld3.fig_to_html(fig)
 
 @app.route('/')
 @app.route('/index')
@@ -24,22 +48,7 @@ def index():
 
 @app.route('/fig')
 def fig():
-    # These coordinates could be stored in DB
-    x1 = [0, 1, 2, 3, 4]
-    y1 = [10, 30, 40, 5, 50]
-    x2 = [0, 1, 2, 3, 4]
-    y2 = [50, 30, 20, 10, 50]
-    x3 = [0, 1, 2, 3, 4]
-    y3 = [0, 30, 10, 5, 30]
-
-    graph1_url = build_graph(x1, y1);
-    graph2_url = build_graph(x2, y2);
-    graph3_url = build_graph(x3, y3);
-
-    return render_template('graphs.html',
-                           graph1=graph1_url,
-                           graph2=graph2_url,
-                           graph3=graph3_url)
+    return draw_fig(data["line"])
 
 
 # if __name__ == '__main__':
