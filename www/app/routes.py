907004fa54ab78750699ff783527d9960752fa1b
dima <knawell@gmail.com>
2018-09-18 00:18:01 +0300

    minor editions in  the flask

--- www/app/routes.py
+++ www/app/routes.py
@@ -1,45 +1,10 @@
 from app import app
 from flask import render_template
-import mpld3
-import matplotlib
-
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-plt.ioff()
+import io
+import base64
 
-import random
 
-
-x = range(100)
-y = [a * 2 + random.randint(-20, 20) for a in x]
-
-def draw_fig(fig_type):
-    """Returns html equivalent of matplotlib figure
-    Parameters
-    ----------
-    fig_type: string, type of figure
-            one of following:
-                    * line
-                    * bar
-    Returns
-    --------
-    d3 representation of figure
-    """
-
-    fig, ax = plt.subplots()
-    if fig_type == "line":
-        ax.plot(x, y)
-    elif fig_type == "bar":
-        ax.bar(x, y)
-    elif fig_type == "scatter":
-        ax.scatter(x, y)
-    elif fig_type == "hist":
-        ax.hist(y, 10, normed=1)
-    elif fig_type == "area":
-        ax.plot(x, y)
-        ax.fill_between(x, 0, y, alpha=0.2)
-
-    return mpld3.fig_to_html(fig)
 
 @app.route('/')
 @app.route('/index')
@@ -50,9 +15,19 @@ def index():
 
 @app.route('/fig')
 def fig():
-    return draw_fig("line")
 
+    img = io.BytesIO()
 
-# if __name__ == '__main__':
-#     app.debug = True
-#     app.run()
+    y = [1, 2, 3, 4, 5]
+    x = [0, 2, 1, 3, 4]
+    plt.plot(x, y)
+    plt.savefig(img, format='png')
+    img.seek(0)
+
+    plot_url = base64.b64encode(img.getvalue()).decode()
+
+    return '<img src="data:image/png;base64,{}">'.format(plot_url)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
