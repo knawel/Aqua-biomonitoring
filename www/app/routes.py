a053b3d97ef52b67f5c0f989f8ad336cd85eb7d1
dima <knawell@gmail.com>
2018-09-17 21:57:02 +0300

    matplotlib corrections

--- www/app/routes.py
+++ www/app/routes.py
@@ -1,7 +1,19 @@
 from app import app
 from flask import render_template
 from flask import Flask, render_template
-from graph import build_graph
+import matplotlib.pyplot as plt
+import io
+import base64
+
+
+def build_graph(x_coordinates, y_coordinates):
+    img = io.BytesIO()
+    plt.plot(x_coordinates, y_coordinates)
+    plt.savefig(img, format='png')
+    img.seek(0)
+    graph_url = base64.b64encode(img.getvalue()).decode()
+    plt.close()
+    return 'data:image/png;base64,{}'.format(graph_url)
 
 @app.route('/')
 @app.route('/index')
