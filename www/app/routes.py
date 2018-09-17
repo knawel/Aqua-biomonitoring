825f5d3227e3bebfd02214b1c201def60d9fc576
dima <knawell@gmail.com>
2018-09-17 21:39:26 +0300

    add matplotlib

--- www/app/routes.py
+++ www/app/routes.py
@@ -7,3 +7,22 @@ from flask import render_template
 def index():
     user = {'username': 'Miguel'}
     return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/fig')
+def fig():
+    fig, ax = plt.subplots()
+    Ntotal = 1000
+    N, bins, patches = ax.hist(np.random.rand(Ntotal), 20)
+
+    fracs = N.astype(float)/N.max()
+    norm = colors.Normalize(fracs.min(), fracs.max())
+
+    for thisfrac, thispatch in zip(fracs, patches):
+        color = cm.jet(norm(thisfrac))
+        thispatch.set_facecolor(color)
+
+    img = io.BytesIO()
+    fig.savefig(img)
+    img.seek(0)
+    return send_file(img, mimetype='image/png')
